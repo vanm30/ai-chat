75ae421d5acbe9d1df51f29a439d6d86f3fea022
Matej <vanikmatej99@gmail.com>
2025-01-02 20:01:35 +0000

    possible colors

--- frontend/src/components/CardGradient.tsx
+++ frontend/src/components/CardGradient.tsx
@@ -5,7 +5,7 @@ interface ICardGradient {
 export default function CardGradient({ children }: ICardGradient) {
     return (
         <div
-            className="flex flex-col flex-1 gap-8 items-center sm:items-start p-1 rounded-xl p-px w-full"
+            className="flex flex-col flex-1 gap-8 items-center sm:items-start p-1 rounded-xl w-full p-[4px]"
             style={{
                 background: `linear-gradient(to bottom, rgba(var(--custom-blue), 0.7), rgba(var(--custom-purple), 0.7), rgba(var(--custom-pink), 0.7))`,
             }}
@@ -13,7 +13,7 @@ export default function CardGradient({ children }: ICardGradient) {
             <div
                 className="rounded-xl p-4 flex-1 w-full backdrop-blur-xxl"
                 style={{
-                    backgroundColor: 'rgba(var(--background), 0.65'
+                    backgroundColor: 'rgba(var(--background), 0.7'
                 }}
             >
                 {children}
